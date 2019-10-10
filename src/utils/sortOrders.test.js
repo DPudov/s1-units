23914cc6678258bf58f55c09f73bfb59373be3cc
Dmitry Pudov <dimapudov99@gmail.com>
2019-10-10 19:58:32 +0300

    Sort orders by items count

--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortTypes, sortByItemNames,
+	sortByItemCount, sortByDate, sortOrders} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
 		const result = sortByItemCount(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('orders are not objects', () => {
+		const result = sortByItemCount(1, 3);
 		expect(result).toEqual(0);
 	});
 
@@ -17,8 +23,107 @@ describe('sortByItemCount function', () => {
 		};
 
 		const result = sortByItemCount(order1, order2);
-
+		
 		expect(result).toBe(0);
+	});
+
+	it('first bigger', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3', 'item4'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('second bigger', () => {
+		const order2 = {
+			items: ['item1', 'item2', 'item3', 'item4'],
+		};
+
+		const order1 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(-1);
 	});
 });
 
+describe('sortOrders function', () => {
+	it('orders are null', () => {
+		const result = sortOrders(null, sortTypes.ITEM_NAMES);
+		expect(result).toBe(undefined);
+	})
+
+	it('orders with no type', () => {
+		const order1 = {
+			items: ['1', '2']
+		};
+
+		const order2 = {
+			items: ['3', '4']
+		};
+
+		const orders = [order1, order2];
+
+		const result = sortOrders(orders, null);
+		expect(result).toBe(undefined);
+
+		expect(orders).toEqual(orders);
+	});
+
+
+	it('orders are null, no type', () => {
+		const result = sortOrders(null, null);
+		expect(result).toBe(undefined);
+	});
+
+	it('sort by names', () => {
+		const order1 = {
+			items: ['2', '1']
+		};
+
+		const order2 = {
+			items: ['3', '4', '1']
+		};
+
+		const orders = [order2, order1];
+
+		const valid = [order1, order2];
+
+		const result = sortOrders(orders, sortTypes.ITEM_NAMES);
+		expect(orders).toEqual(valid);
+	});
+
+	it('sort by date', () => {
+		const order1 = {
+			items: ['2', '1']
+		};
+
+		const order2 = {
+			items: ['3', '4', '1']
+		};
+
+		const orders = [order2, order1];
+
+		const valid = [
+			{
+				items: ['2', '1']
+			},
+			{
+				items: ['3', '4', '1']
+			}
+		];
+
+		const result = sortOrders(orders, sortTypes.DATE);
+		expect(orders).toEqual(valid);
+	});
+
+});
